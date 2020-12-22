d55b06ce9b27a43b89404e39a0e510994c0014ef
Talha Imran <talhaimran284@gmail.com>
2020-12-22 18:50:44 +0500

    Used error handling to show alerts to the user if they enter mismatch passwords

--- signup.php
+++ signup.php
@@ -1,3 +1,34 @@
+<?php
+
+    $error = false;
+    $errorMessage = "";
+    session_start();
+
+    // Including code from these files
+    include("./connection.php");
+    include("./functions.php");
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $user_name =  $_POST["user_name"];
+        $password =  $_POST["password"];
+        $password_copy =  $_POST["password_copy"];
+
+        if(!empty($user_name) && !empty($password) && !empty($password_copy)){
+            if($password == $password_copy){
+                
+            }
+            else{
+                $error = true;
+                $errorMessage = "Passwords do not match!";
+            }
+        }
+        else{
+            $error = true;
+            $errorMessage = "Cannot leave any of the fields empty!";
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,38 +40,63 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
+    <!-- Font Awsome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+        crossorigin="anonymous" />
+
     <title>Document</title>
 </head>
 
 <body>
-    <div class="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto my-5">
+    <div class="col-10 col-sm-8 col-md-6 col-lg-5 mx-auto my-5">
 
-        <div class="container text-center mb-4">
-            <h2 class="text-primary">Gym Registration Website</h2>
-        </div>
+        <!-- <div class="container mb-4">
+            <h1 class="text-primary text-center">Gym Registration Website</h1>
+        </div> -->
         <div class="card card-body bg-light">
-            <h3 class="text-center mb-2 text-uppercase text-primary">Sign Up Form</h3>
-            <h5 class="text-center mb-2 text-uppercase">For Gym Admins</h5>
+            <div class="row">
+                <div class="col-8 align-self-center">
+                    <h4 class="text-left mb-2 text-uppercase text-primary">Sign Up Form</h4>
+                    <p class="text-left mb-2 strong">Sign up and start registering your customers.</p>
+                </div>
+                <div class="col-4 align-self-center text-center">
+                    <i class="fa fa-user-plus fa-3x text-primary" aria-hidden="true"></i>
+                </div>
+            </div>
             <hr />
-            <form>
+
+            <?php
+                if($error){ ?>
+
+            <div class="alert alert-danger alert-dismissible fade show">
+                <button type="button" class="btn btn-danger close mr-4" data-dismiss="alert">&times;</button>
+                <?php echo $errorMessage ?>
+            </div><?php
+            }
+            ?>
+            <form method="POST">
                 <label for="user_name">Enter user name</label>
-                <input type="text" class="form-control" autocomplete=false placeholder="Enter username...">
+                <input type="text" class="form-control" autocomplete="false" placeholder="Enter username..."
+                    required="true" name="user_name">
                 <br />
                 <label for="password">Enter password</label>
-                <input type="password" class="form-control" autocomplete=false placeholder="Enter password...">
+                <input type="password" class="form-control" autocomplete="false" placeholder="Enter password..."
+                    required="true" name="password">
                 <br />
                 <label for="password_copy">Re Enter the password</label>
-                <input type="password" class="form-control" autocomplete=false placeholder="Re enter password...">
+                <input type="password" class="form-control" autocomplete="false" placeholder="Re enter password..."
+                    required="true" name="password_copy">
                 <br />
                 <br />
-                <a href="login.php" class="btn btn-outline-success d-block">SignUp</a>
+                <button type="submit" class="w-100 btn btn-block btn-outline-primary">SignUp</button>
             </form>
         </div>
     </div>
     <!-- For Bootstrap to work perfectly -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous">
-    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
