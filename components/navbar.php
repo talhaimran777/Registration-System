30dbc22e64f7a3ccec49c4ecc2dda22e118c8255
Talha Imran <talhaimran284@gmail.com>
2020-12-23 07:52:23 +0500

    Made navbar component reusable

---
+++ components/navbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container px-5">
+        <a class="navbar-brand" href="#">Gym Registration</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav" style="margin-left: auto;">
+                <li class="nav-item active">
+                    <a class="nav-link"><?php echo($user_data["user_name"])?></a>
+                </li>
+
+                <li class="nav-item active">
+                    <a class="nav-link" href="dashboard.php">Dashboard<span class="sr-only">(current)</span></a>
+                </li>
+
+                <li class="nav-item active">
+                    <a class="nav-link" href="reg_customer.php">Register Customer<span
+                            class="sr-only">(current)</span></a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- dashboard.php
+++ dashboard.php
@@ -10,14 +10,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <!-- Font Awsome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+        crossorigin="anonymous" />
     <title>Dashboard</title>
 </head>
 
 <body>
-    <h1>This is a dashboard!</h1>
-    <h3>Hello, <?php  echo($user_data["user_name"])?></h3>
 
-    <a href="logout.php">Logout</a>
+    <!-- Including navbar component -->
+    <?php
+        include("./components/navbar.php");
+    ?>
+
+    <div class="container px-5">
+        <h3 class="mt-4">Hello, <?php  echo($user_data["user_name"])?></h3>
+    </div>
+
+
+
+
+    <!-- For Bootstrap to work perfectly -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
